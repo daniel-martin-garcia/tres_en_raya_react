92f73d8f3e44bc73839455753290200229ad5796
daniel-martin-garcia <danielmartingarcia@outlook.es>
2017-11-29 20:59:07 +0100

    Board and players added

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,12 +1,42 @@
 import React from 'react';
 import './../assets/scss/main.scss';
 
+const JUGADORX = "Las X"
+const JUGADORY = "Los 0"
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <h2 id="heading">Hello ReactJS</h2>
-    );
+	constructor(props) {
+		super(props);
+		this.state = {
+			turno: JUGADORX,
+			valores: [
+				['-','-','-'],
+				['-','-','-'],
+				['-','-','-'],
+			],
+		};
+	}
+
+	render() {
+		let texto = "Turno de " + this.state.turno;
+		let tablero = this.state.valores.map((valoresFila, indiceFila) => {
+			let fila = valoresFila.map((valor, indiceColumna) => {
+				let myKey = "" + indiceFila + indiceColumna;
+				return(
+					<span key={myKey}> {valor}</span>
+				);
+			});
+			return (
+				<div key={"fila" + indiceFila}>{fila}</div>
+			);
+		});
+		return (
+		  <div>
+		  	<h2 id="heading">Tres en raya</h2>
+		  	<header className="cabecera">{texto}</header>
+		  	{tablero}
+		  </div>
+		);
   }
 
 }
