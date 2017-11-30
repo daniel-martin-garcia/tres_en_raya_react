67a8ac03f13bee20cbaa8458cf0ccd8a69fbcccc
daniel-martin-garcia <danielmartingarcia@outlook.es>
2017-11-30 19:16:03 +0100

    Header component

---
+++ app/components/Cabecera.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class Cabecera extends React.Component {
+	render(){
+		return {
+			<header className="cabecera">{this.props.texto}</header>
+		};
+	}
+}
