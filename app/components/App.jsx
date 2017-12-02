2ed8206e9e693efb1ed7401b48894d824166eb01
daniel-martin-garcia <danielmartingarcia@outlook.es>
2017-12-02 17:56:07 +0100

    Reset added

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './../assets/scss/main.scss';
 import Cabecera from './Cabecera.jsx';
 import Tablero from './Tablero.jsx';
+import Reset from './Reset.jsx';
 
 const JUGADORX = "Las X"
 const JUGADOR0 = "Los 0"
@@ -19,6 +20,20 @@ export default class App extends React.Component {
 			movimientos: 0
 		};
 		this.appClick = this.appClick.bind(this);
+		this.resetClick = this.resetClick.bind(this);
+	}
+
+	resetClick() {
+		this.setState({
+			turno: JUGADORX,
+			valores: [
+				['-','-','-'],
+				['-','-','-'],
+				['-','-','-'],
+			],
+			movimientos: 0
+
+		});
 	}
 
 	appClick(numeroFila, numeroColumna) {
@@ -39,6 +54,7 @@ export default class App extends React.Component {
 		  	<Cabecera texto={texto}/>
 		  	<Tablero valores={this.state.valores} appClick={this.appClick}/>
 		  	<h3> Numero de movimientos: {this.state.movimientos}</h3>
+		  	<Reset resetClick={this.resetClick}/>
 		  </div>
 		);
   }
